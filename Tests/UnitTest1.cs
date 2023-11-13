8df0f6d810433bf9678ba3020bfff85fbdedd648
Oskar Dudycz <oskar.dudycz@gmail.com>
2023-11-13 14:26:53 +0100

    Updated with the (almost) full implementation

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,17 +1,269 @@
+using System.Collections.Immutable;
+using Microsoft.EntityFrameworkCore;
+
 namespace Tests;
 
-public static class ListExtensions
+using static ShoppingCartCommand;
+using static ShoppingCartEvent;
+using static ShoppingCart;
+using ProductItems = ImmutableDictionary<string, int>;
+
+public record ProductItem(Guid ProductId, int Quantity);
+
+public record PricedProductItem(Guid ProductId, int Quantity, decimal Price):
+    ProductItem(ProductId, Quantity);
+
+// Commands
+public abstract record ShoppingCartCommand
 {
-    public static IList<T> Replace<T>(this IList<T> list, T existingElement, T replacement)
+    public record AddProduct(
+        Guid CartId,
+        ProductItem ProductItem,
+        Guid? ClientId = null
+    ): ShoppingCartCommand;
+
+    public record RemoveProduct(
+        Guid CartId,
+        PricedProductItem ProductItem
+    ): ShoppingCartCommand;
+
+    public record Confirm(
+        Guid CartId,
+        Guid ClientId
+    ): ShoppingCartCommand;
+
+    public record Cancel(
+        Guid CartId
+    ): ShoppingCartCommand;
+}
+
+public abstract record ShoppingCartEvent
+{
+    public record Opened(
+        Guid CartId,
+        Guid? ClientId,
+        DateTimeOffset OpenedAt
+    ): ShoppingCartEvent;
+
+    public record ProductAdded(
+        Guid CartId,
+        PricedProductItem ProductItem,
+        DateTimeOffset AddedAt
+    ): ShoppingCartEvent;
+
+    public record ProductRemoved(
+        Guid CartId,
+        PricedProductItem ProductItem,
+        DateTimeOffset RemovedAt
+    ): ShoppingCartEvent;
+
+    public record Confirmed(
+        Guid CartId,
+        Guid ClientId,
+        DateTimeOffset ConfirmedAt
+    ): ShoppingCartEvent;
+
+    public record Cancelled(
+        Guid CartId,
+        DateTimeOffset CanceledAt
+    ): ShoppingCartEvent;
+}
+
+public abstract record ShoppingCart
+{
+    public record Empty: ShoppingCart;
+
+    public record Pending(ProductItems ProductItems): ShoppingCart;
+
+    public record Closed: ShoppingCart;
+}
+
+public class ShoppingClassDecider(
+    IProductPriceCalculator priceCalculator,
+    TimeProvider timeProvider)
+{
+    public ShoppingCartEvent[] Decide(ShoppingCartCommand command, ShoppingCart state) =>
+        (command, state) switch
+        {
+            (AddProduct (var cartId, var productItem, var clientId), Empty) =>
+            [
+                new Opened(cartId, clientId, Now),
+                new ProductAdded(cartId, priceCalculator.Calculate(productItem).Single(), Now)
+            ],
+
+            (AddProduct (var cartId, var productItem, _), Pending) =>
+            [
+                new ProductAdded(cartId, priceCalculator.Calculate(productItem).Single(), Now)
+            ],
+
+            (RemoveProduct(var cartId, var pricedProductItem), Pending pending) =>
+            [
+                new ProductRemoved(cartId, pricedProductItem, Now)
+            ],
+
+            (Confirm(var cartId, var clientId), Pending pending) =>
+            [
+                new Confirmed(cartId, clientId, Now)
+            ],
+
+            (Confirm, Closed) => [],
+
+
+            (Cancel(var cartId), Pending pending) =>
+            [
+                new Cancelled(cartId, Now)
+            ],
+
+            (Cancel, Closed) => [],
+
+            (_, _) => throw new InvalidOperationException(nameof(command))
+        };
+
+    private DateTimeOffset Now => timeProvider.GetLocalNow();
+}
+
+public class ShoppingCartModel
+{
+    public required Guid Id { get; init; }
+    public Guid? ClientId { get; set; }
+    public required ShoppingCartStatus Status { get; set; }
+    public List<PricedProductItem> ProductItems { get; init; } = [];
+    public required DateTimeOffset OpenedAt { get; set; }
+    public DateTimeOffset? ConfirmedAt { get; set; }
+    public DateTimeOffset? CanceledAt { get; set; }
+
+    public class PricedProductItem
     {
-        var indexOfExistingItem = list.IndexOf(existingElement);
+        public required Guid ProductId { get; init; }
+        public required int Quantity { get; set; }
+        public required decimal Price { get; init; }
+    }
 
-        if (indexOfExistingItem == -1)
-            throw new ArgumentOutOfRangeException(nameof(existingElement), "Element was not found");
+    public enum ShoppingCartStatus
+    {
+        Pending,
+        Confirmed,
+        Canceled
+    }
+}
 
-        list[indexOfExistingItem] = replacement;
+public interface IShoppingCartRepository
+{
+    ValueTask<ShoppingCartModel?> Find(Guid id, CancellationToken ct);
+    Task Store(Guid id, ShoppingCartModel? current, ShoppingCartEvent[] events, CancellationToken ct);
 
-        return list;
+    async Task GetAndStore(Guid id, Func<ShoppingCartModel?, ShoppingCartEvent[]> handle, CancellationToken ct)
+    {
+        var state = await Find(id, ct);
+
+        var events = handle(state);
+
+        await Store(id, state, events, ct);
+    }
+}
+
+public class ShoppingClassCommandHandler(
+    ShoppingClassDecider decider,
+    IShoppingCartRepository repository
+)
+{
+    public Task Handle(Guid id, ShoppingCartCommand command, CancellationToken ct) =>
+        repository.GetAndStore(
+            id,
+            model => decider.Decide(command, Map(model)),
+            ct
+        );
+
+    private ShoppingCart Map(ShoppingCartModel? model)
+    {
+        if (model == null)
+            return new Empty();
+
+        switch (model.Status)
+        {
+            case ShoppingCartModel.ShoppingCartStatus.Pending:
+                return new Pending(
+                    model.ProductItems.ToImmutableDictionary(ks => $"{ks.ProductId}__{ks.Price}", vs => vs.Quantity)
+                );
+
+            case ShoppingCartModel.ShoppingCartStatus.Confirmed:
+            case ShoppingCartModel.ShoppingCartStatus.Canceled:
+                return new Closed();
+
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
+
+public class ECommerceDBContext: DbContext
+{
+    public DbSet<ShoppingCartModel> ShoppingCarts { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ShoppingCartModel>()
+            .ToTable("ShoppingCarts")
+            .OwnsMany(b => b.ProductItems, a =>
+            {
+                a.ToTable("ProductItems");
+                a.WithOwner().HasForeignKey("ShoppingCartId");
+                a.HasKey("ProductId", "ShoppingCartId");
+            });
+    }
+}
+
+public class ShoppingCartRepository(ECommerceDBContext dbContext): IShoppingCartRepository
+{
+    public ValueTask<ShoppingCartModel?> Find(Guid id, CancellationToken ct) =>
+        dbContext.FindAsync<ShoppingCartModel>([id], ct);
+
+    public Task Store(Guid id, ShoppingCartModel? current, ShoppingCartEvent[] events, CancellationToken ct)
+    {
+        foreach (var @event in events)
+        {
+            switch (@event)
+            {
+                case Opened(_, var clientId, var openedAt):
+                    dbContext.Add(new ShoppingCartModel
+                    {
+                        Id = id,
+                        ClientId = clientId,
+                        Status = ShoppingCartModel.ShoppingCartStatus.Pending,
+                        OpenedAt = openedAt
+                    });
+                    break;
+                case Confirmed (_, var clientId, var confirmedAt):
+                    current!.Status = ShoppingCartModel.ShoppingCartStatus.Confirmed;
+                    current.ClientId = clientId;
+                    current.ConfirmedAt = confirmedAt;
+                    dbContext.ShoppingCarts.Update(current);
+                    break;
+                case Cancelled (_, var canceledAt):
+                    current!.Status = ShoppingCartModel.ShoppingCartStatus.Canceled;
+                    current.ConfirmedAt = canceledAt;
+                    dbContext.ShoppingCarts.Update(current);
+                    break;
+                case ProductAdded(_, var productItem, _):
+                    current!.ProductItems
+                        .Single(p => p.ProductId == productItem.ProductId)
+                        .Quantity += productItem.Quantity;
+                    break;
+                case ProductRemoved(_, var productItem, _):
+                    var existing = current!.ProductItems.Single(p => p.ProductId == productItem.ProductId);
+
+                    if (existing.Quantity - productItem.Quantity == 0)
+                        current.ProductItems.Remove(existing);
+                    else
+                        existing.Quantity -= productItem.Quantity;
+
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(@event));
+            }
+        }
+
+        return dbContext.SaveChangesAsync(ct);
     }
 }
 
@@ -20,342 +272,11 @@ public interface IProductPriceCalculator
     IReadOnlyList<PricedProductItem> Calculate(params ProductItem[] productItems);
 }
 
-// Commands
-
-public record OpenShoppingCart(
-    Guid CartId,
-    Guid ClientId
-);
-
-public record AddProduct(
-    Guid CartId,
-    ProductItem ProductItem
-);
-
-public record RemoveProduct(
-    Guid CartId,
-    PricedProductItem ProductItem
-);
-
-public record ConfirmShoppingCart(
-    Guid CartId
-);
-
-public record CancelShoppingCart(
-    Guid CartId
-);
-
-// Events
-
-public record ShoppingCartOpened(
-    Guid CartId,
-    Guid ClientId
-);
-
-public record ProductAdded(
-    Guid CartId,
-    PricedProductItem ProductItem
-);
-
-public record ProductRemoved(
-    Guid CartId,
-    PricedProductItem ProductItem
-);
-
-public record ShoppingCartConfirmed(
-    Guid CartId,
-    DateTime ConfirmedAt
-);
-
-public record ShoppingCartCanceled(
-    Guid CartId,
-    DateTime CanceledAt
-);
-
-
-// Value Objects
-public record PricedProductItem
+public class DummyProductPriceCalculator(decimal price): IProductPriceCalculator
 {
-    public Guid ProductId => ProductItem.ProductId;
-
-    public int Quantity => ProductItem.Quantity;
-
-    public decimal UnitPrice { get; }
-
-    public decimal TotalPrice => Quantity * UnitPrice;
-    public ProductItem ProductItem { get; }
-
-    private PricedProductItem(ProductItem productItem, decimal unitPrice)
-    {
-        ProductItem = productItem;
-        UnitPrice = unitPrice;
-    }
-
-    public static PricedProductItem Create(Guid? productId, int? quantity, decimal? unitPrice)
-    {
-        return Create(
-            ProductItem.From(productId, quantity),
-            unitPrice
-        );
-    }
-
-    public static PricedProductItem Create(ProductItem productItem, decimal? unitPrice)
-    {
-        return unitPrice switch
-        {
-            null => throw new ArgumentNullException(nameof(unitPrice)),
-            <= 0 => throw new ArgumentOutOfRangeException(nameof(unitPrice),
-                "Unit price has to be positive number"),
-            _ => new PricedProductItem(productItem, unitPrice.Value)
-        };
-    }
-
-    public bool MatchesProductAndPrice(PricedProductItem pricedProductItem)
-    {
-        return ProductId == pricedProductItem.ProductId && UnitPrice == pricedProductItem.UnitPrice;
-    }
-
-    public PricedProductItem MergeWith(PricedProductItem pricedProductItem)
-    {
-        if (!MatchesProductAndPrice(pricedProductItem))
-            throw new ArgumentException("Product or price does not match.");
-
-        return new PricedProductItem(ProductItem.MergeWith(pricedProductItem.ProductItem), UnitPrice);
-    }
-
-    public PricedProductItem Subtract(PricedProductItem pricedProductItem)
-    {
-        if (!MatchesProductAndPrice(pricedProductItem))
-            throw new ArgumentException("Product or price does not match.");
-
-        return new PricedProductItem(ProductItem.Subtract(pricedProductItem.ProductItem), UnitPrice);
-    }
-
-    public bool HasEnough(int quantity)
-    {
-        return ProductItem.HasEnough(quantity);
-    }
-
-    public bool HasTheSameQuantity(PricedProductItem pricedProductItem)
-    {
-        return ProductItem.HasTheSameQuantity(pricedProductItem.ProductItem);
-    }
+    public IReadOnlyList<PricedProductItem> Calculate(params ProductItem[] productItems) =>
+        productItems.Select(pi => new PricedProductItem(pi.ProductId, pi.Quantity, price)).ToArray();
 }
-
-public record ProductItem
-{
-    public Guid ProductId { get; }
-
-    public int Quantity { get; }
-
-    private ProductItem(Guid productId, int quantity)
-    {
-        ProductId = productId;
-        Quantity = quantity;
-    }
-
-    public static ProductItem From(Guid? productId, int? quantity)
-    {
-        if (!productId.HasValue)
-            throw new ArgumentNullException(nameof(productId));
-
-        return quantity switch
-        {
-            null => throw new ArgumentNullException(nameof(quantity)),
-            <= 0 => throw new ArgumentOutOfRangeException(nameof(quantity), "Quantity has to be a positive number"),
-            _ => new ProductItem(productId.Value, quantity.Value)
-        };
-    }
-
-    public ProductItem MergeWith(ProductItem productItem)
-    {
-        if (!MatchesProduct(productItem))
-            throw new ArgumentException("Product does not match.");
-
-        return From(ProductId, Quantity + productItem.Quantity);
-    }
-
-    public ProductItem Subtract(ProductItem productItem)
-    {
-        if (!MatchesProduct(productItem))
-            throw new ArgumentException("Product does not match.");
-
-        return From(ProductId, Quantity - productItem.Quantity);
-    }
-
-    public bool MatchesProduct(ProductItem productItem)
-    {
-        return ProductId == productItem.ProductId;
-    }
-
-    public bool HasEnough(int quantity)
-    {
-        return Quantity >= quantity;
-    }
-
-    public bool HasTheSameQuantity(ProductItem productItem)
-    {
-        return Quantity == productItem.Quantity;
-    }
-}
-
-// Aggregate
-
-public enum ShoppingCartStatus
-{
-    Pending = 1,
-    Confirmed = 2,
-    Canceled = 4
-}
-
-public class ShoppingCart
-{
-    public Guid Id { get; private set; }
-
-    public Guid ClientId { get; private set; }
-
-    public ShoppingCartStatus Status { get; private set; }
-
-    public IList<PricedProductItem> ProductItems { get; private set; } = default!;
-
-    public decimal TotalPrice => ProductItems.Sum(pi => pi.TotalPrice);
-
-    public static (ShoppingCartOpened, ShoppingCart) Open(
-        Guid cartId,
-        Guid clientId)
-    {
-        var cart = new ShoppingCart();
-
-        var @event = new ShoppingCartOpened(
-            cartId,
-            clientId
-        );
-        cart.Apply(@event);
-
-        return (@event, cart);
-    }
-
-    public void Apply(ShoppingCartOpened @event)
-    {
-        Id = @event.CartId;
-        ClientId = @event.ClientId;
-        ProductItems = new List<PricedProductItem>();
-        Status = ShoppingCartStatus.Pending;
-    }
-
-    public void AddProduct(
-        IProductPriceCalculator productPriceCalculator,
-        ProductItem productItem)
-    {
-        if(Status != ShoppingCartStatus.Pending)
-            throw new InvalidOperationException($"Adding product for the cart in '{Status}' status is not allowed.");
-
-        var pricedProductItem = productPriceCalculator.Calculate(productItem).Single();
-
-        var @event = new ProductAdded(Id, pricedProductItem);
-
-        Apply(@event);
-    }
-
-    public void Apply(ProductAdded @event)
-    {
-        var newProductItem = @event.ProductItem;
-
-        var existingProductItem = FindProductItemMatchingWith(newProductItem);
-
-        if (existingProductItem is null)
-        {
-            ProductItems.Add(newProductItem);
-            return;
-        }
-
-        ProductItems.Replace(
-            existingProductItem,
-            existingProductItem.MergeWith(newProductItem)
-        );
-    }
-
-    public void RemoveProduct(
-        PricedProductItem productItemToBeRemoved)
-    {
-        if(Status != ShoppingCartStatus.Pending)
-            throw new InvalidOperationException($"Removing product from the cart in '{Status}' status is not allowed.");
-
-        var existingProductItem = FindProductItemMatchingWith(productItemToBeRemoved);
-
-        if (existingProductItem is null)
-            throw new InvalidOperationException($"Product with id `{productItemToBeRemoved.ProductId}` and price '{productItemToBeRemoved.UnitPrice}' was not found in cart.");
-
-        if(!existingProductItem.HasEnough(productItemToBeRemoved.Quantity))
-            throw new InvalidOperationException($"Cannot remove {productItemToBeRemoved.Quantity} items of Product with id `{productItemToBeRemoved.ProductId}` as there are only ${existingProductItem.Quantity} items in card");
-
-        var @event = new ProductRemoved(Id, productItemToBeRemoved);
-
-        Apply(@event);
-    }
-
-    public void Apply(ProductRemoved @event)
-    {
-        var productItemToBeRemoved = @event.ProductItem;
-
-        var existingProductItem = FindProductItemMatchingWith(@event.ProductItem);
-
-        if (existingProductItem == null)
-            return;
-
-        if (existingProductItem.HasTheSameQuantity(productItemToBeRemoved))
-        {
-            ProductItems.Remove(existingProductItem);
-            return;
-        }
-
-        ProductItems.Replace(
-            existingProductItem,
-            existingProductItem.Subtract(productItemToBeRemoved)
-        );
-    }
-
-    public void Confirm()
-    {
-        if(Status != ShoppingCartStatus.Pending)
-            throw new InvalidOperationException($"Confirming cart in '{Status}' status is not allowed.");
-
-        if (ProductItems.Count == 0)
-            throw new InvalidOperationException($"Confirming empty cart is not allowed.");
-
-        var @event = new ShoppingCartConfirmed(Id, DateTime.UtcNow);
-
-        Apply(@event);
-    }
-
-    public void Apply(ShoppingCartConfirmed @event)
-    {
-        Status = ShoppingCartStatus.Confirmed;
-    }
-
-    public void Cancel()
-    {
-        if(Status != ShoppingCartStatus.Pending)
-            throw new InvalidOperationException($"Canceling cart in '{Status}' status is not allowed.");
-
-        var @event = new ShoppingCartCanceled(Id, DateTime.UtcNow);
-
-        Apply(@event);
-    }
-
-    public void Apply(ShoppingCartCanceled @event)
-    {
-        Status = ShoppingCartStatus.Canceled;
-    }
-
-    private PricedProductItem? FindProductItemMatchingWith(PricedProductItem productItem)
-    {
-        return ProductItems
-            .SingleOrDefault(pi => pi.MatchesProductAndPrice(productItem));
-    }
-}
-
-
 
 public class UnitTest1
 {
